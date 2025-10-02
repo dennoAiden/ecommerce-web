6f01a8f44677e64a23d7d33ec4dc7c5f5dc11ff8
dennis <kipkiruidennis25@gmail.com>
2025-10-02 11:52:22 +0300

    updating cors in my server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
+
+const allowedOrigins = [
+  "https://ecommerce-website-ffcq.onrender.com"
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true, 
+}));
 
 app.get("/", (req, res) => {
   res.send("Backend API running 🚀");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on https://ecommerce-web-1-vlvp.onrender.com:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
